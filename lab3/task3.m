d723dacc706717d64b030f97021458a0be9d6360
doctor_mee <wandrsl@gmail.com>
2017-11-28 20:13:42 +0300

    Commented approximation

--- lab3/task3.m
+++ lab3/task3.m
@@ -6,7 +6,7 @@ sqn = squares(N);
 sn = cumsum(sqn);
 plot(N, sn - s);
 hold on;
-psi = @(n) 1./n;
+psi = @(n) 1./n; %from the fact that Σ(1./n.^2) <= Σ(1./n(n - 1))
 plot(N, psi(N));
 grid on;
 legend('sn - s', 'psi');
